3b65fa772fec69dc248de9dea212cd10d05e33ac
hckang80 <hckang80@gmail.com>
2022-03-25 23:33:37 +0900

    feat: Add to search keyword function and relates

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,9 +1,31 @@
+import React, { useState } from 'react'
+import useAsync, { type AsyncState } from '@/composables/useAsync'
+import useFunction from '@/composables/useFunction'
+
 export default function Search() {
+  const BASE_URI = 'https://wr4a6p937i.execute-api.ap-northeast-2.amazonaws.com/dev'
+
+  const { request } = useFunction()
+
+  const [keyword, setKeyword] = useState('')
+
+  const inputKeyword = async ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(target.value)
+    const response = await request(`${BASE_URI}/languages?keyword=${target.value}`)
+    return response
+  }
+
   return (
     <main className="Search">
       <div className="selected-list"></div>
       <form className="search-form">
-        <input type="text" placeholder="" className="search-form__input" />
+        <input
+          type="text"
+          placeholder=""
+          className="search-form__input"
+          onChange={inputKeyword}
+          value={keyword}
+        />
       </form>
     </main>
   )
