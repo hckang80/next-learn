1e7979861b67c13e14c43da0ea1258f4cab42fe6
hckang80 <hckang80@gmail.com>
2022-03-25 23:09:17 +0900

    feat: Init Search page component
    Resolves: #feat

---
+++ pages/search/index.tsx
@@ -0,0 +1,10 @@
+export default function Search() {
+  return (
+    <main className="Search">
+      <div className="selected-list"></div>
+      <form className="search-form">
+        <input type="text" placeholder="" className="search-form__input" />
+      </form>
+    </main>
+  )
+}
