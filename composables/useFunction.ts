3daa59b1506cf2f0c26bf4dd680630a20b3cbfba
hckang80 <hckang80@gmail.com>
2022-03-25 23:32:28 +0900

    feat: Add composable for http request

--- composables/useFunction.ts
+++ composables/useFunction.ts
@@ -1,8 +1,19 @@
 import { debounce, set, throttle } from 'lodash'
 
+export const request = async (url = '', options = {}) => {
+  try {
+    const response = await fetch(url, options)
+    if (response.ok) return await response.json()
+    throw new Error('HTTP REQUEST FAILED')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
 export default () => {
   return {
     debounce,
+    request,
     set,
     throttle
   }
